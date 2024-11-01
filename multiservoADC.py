2c974aa919343ba4b1433db37f8cf1fe631b291e
Noah Marks <19noahm@hollandparkschool.co.uk>
2024-11-01 22:35:07 +0000

    New test with 3 pots for 3 servos

--- multiservoADC.py
+++ multiservoADC.py
@@ -15,21 +15,26 @@ def clamp(val, out_min, out_max):
     
 def map(value, in_min, in_max, out_min, out_max):
     val = ((out_max-out_min) * (value-in_min))/(in_max-in_min) + out_min
-    val = clamp(val, out_min, out_max)
-    
+    val = clamp(val, out_min, out_max)    
     return val
+
+max_pot_volt = 3.3
+min_pot_volt = 0.0
+
 i2c = busio.I2C(board.SCL,board.SDA)
 
 ads = ADS.ADS1115(i2c,address=0x48)
 # ads.gain = 1
 pot0 = AnalogIn(ads, ADS.P0)
 pot1 = AnalogIn(ads, ADS.P1)
+pot2 = AnalogIn(ads, ADS.P2)
 
-max_pot_volt = 3.3
-min_pot_volt = 0.0
 servo0 = Servo(17, initial_value = 0, min_pulse_width = 0.5/1000, max_pulse_width = 2.5/1000)
+servo1 = Servo(27, initial_value = 0, min_pulse_width = 0.5/1000, max_pulse_width = 2.5/1000)
+servo2 = Servo(22, initial_value = 0, min_pulse_width = 0.5/1000, max_pulse_width = 2.5/1000)
 
 while True:
-        v = map(pot0.voltage, max_pot_volt, min_pot_volt, -1, 1)
-        servo0.value = v
+        servo0.value = map(pot0.voltage, max_pot_volt, min_pot_volt, -1, 1)
+        servo1.value = map(pot1.voltage, max_pot_volt, min_pot_volt, -1, 1)
+        servo2.value = map(pot2.voltage, max_pot_volt, min_pot_volt, -1, 1)
         time.sleep(0.1)
